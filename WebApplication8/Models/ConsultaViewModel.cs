3d362b54382f16c30b02d38d4f11dc60f017fede
Antony Ventura <antony28.94@gmail.com>
2020-10-09 08:24:02 -0400

    Series Negadas Base de datos y Frontend

---
+++ WebApplication8/Models/ConsultaViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication8.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication8.Models
+{
+    public class ConsultaViewModel
+    {
+        public IEnumerable<items> items { get; set; }
+
+        public int Accion { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime fechaDesde { get; set; }
+
+        [DataType(DataType.Date)]
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime fechaHasta { get; set; }
+    }
+
+}
